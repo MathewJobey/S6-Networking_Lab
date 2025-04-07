0b5f468ec9d7309898e81f53c4e4811049878723
MathewJobey <mathewjobey17@gmail.com>
2025-04-07 21:24:39 +0530

    TCP

--- TCP/client.c
+++ TCP/client.c
@@ -1,73 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+int main()
+{
 
-    //CREATE CLIENT socket
-    int client_socket=socket(AF_INET,SOCK_STREAM,0);//a file descriptor is returned (+ve integer)
-    if(client_socket<0)
+    // CREATE CLIENT socket
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0); // a file descriptor is returned (+ve integer)
+    if (client_socket < 0)
     {
         printf("ERROR(socket creation)\n");
+        return -1;
     }
     else
     {
         printf("socket created successfully\n");
     }
 
-    //create socket address structure that we need to connect to 
+    // create socket address structure that we need to connect to
     struct sockaddr_in server_addr;
-    server_addr.sin_family= AF_INET;
-    server_addr.sin_port=htons(2000);
-    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    //CONNECT to the SERVER
-    int connection=connect(client_socket_desc,(struct sockaddr*)&server_addr,sizeof(server_addr));
-    if(connection<0)
+    // CONNECT to the SERVER
+    int connection = connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (connection < 0)
     {
-        printf("Connection Unsuccessfull\n");
+        printf("ERROR(connect)\n");
+        return -1;
     }
     else
     {
         printf("Connected to server\n");
     }
-    
-    char client_msg[2000],server_msg[2000];
-    //clear buffers
-    while(1)
-    {
-        memset(server_msg,'\0',sizeof(server_msg));
-        memset(client_msg,'\0',sizeof(client_msg));
 
-        //SEND data to SERVER
+    char client_msg[2000], server_msg[2000];
+    while (1)
+    {
+        // clear buffers
+        memset(server_msg, '\0', sizeof(server_msg));
+        memset(client_msg, '\0', sizeof(client_msg));
+
+        // SEND data to SERVER
         printf("Enter Message: ");
         gets(client_msg);
-        //send(socket_fd, buffer, length_of_data, flags) | returns no.of bytes of the msg sent
-        int sent=send(client_socket,client_msg,sizeof(client_msg),0);
-        if (sent<0)
+        // send(socket_fd, buffer, length_of_data, flags) | returns no.of bytes of the msg sent
+        int sent = send(client_socket, client_msg, sizeof(client_msg), 0);
+        if (sent < 0)
         {
-            printf("ERROR\n");
+            printf("ERROR(send)\n");
         }
-        else
+        // recv data from SERVER
+        int recvd = recv(client_socket, server_msg, sizeof(server_msg), 0);
+        if (recvd < 0)
         {
-            printf("msg sent!\n");
+            printf("ERROR(recv)\n");
         }
-        //receive data from SERVER
-        int recvd=recv(client_socket,server_msg,sizeof(server_msg),0);
-        if(recvd<0)
-        {
-            printf("ERROR\n");
-        }
-        else
-        {
-            printf("msg recvd!\n");
-        }
-        printf("SERVER's response: %s\n",server_msg);
-        if(strcmp(server_msg,"exit")==0)    
+        printf("SERVER's response: %s\n", server_msg);
+        if (strcmp(server_msg, "exit") == 0)
             break;
-        } 
-    close(client_socket_desc);
-    return 0
+    }
+    close(client_socket);
+    return 0;
 }
+
+/*
+    As a CLIENT:
+    - We don't need to manually define our own IP and port.
+    - The OS assigns them automatically (any free IP/port available).
+    - We ONLY need to know the SERVER's:
+        - IP address (e.g., "127.0.0.1")
+        - Port number (e.g., 2000)
+    - Then, we use connect() to reach that specific server address.
+
+    Summary:
+    CLIENT -> connects TO -> SERVER_IP : SERVER_PORT
+*/
--- TCP/server.c
+++ TCP/server.c
@@ -1,56 +1,99 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+int main()
+{
 
-    //CREATE SERVER socket
+    // CREATE SERVER socket
     int server_socket;
-    server_socket=socket(AF_INET,SOCK_STREAM,0);//a file descriptor is returned (+ve integer)
-    if(server_socket<0)
+    server_socket = socket(AF_INET, SOCK_STREAM, 0); // a file descriptor is returned (+ve integer)
+    if (server_socket < 0)
     {
-        printf("ERROR(socket creation)\n");   
+        printf("ERROR(socket creation)\n");
+        return -1;
     }
     else
     {
         printf("socket created successfully\n");
     }
 
-    //define SERVER address
-    struct sockaddr_in server_addr,client_addr;
-    server_addr.sin_family=AF_INET;
-    server_addr.sin_port=htons(2000);
-    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    // define SERVER address
+    struct sockaddr_in server_addr, client_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    //BIND the defined address to the SERVER's socket
-    int binding=bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr));//same as connect parameters
-    if(binding<0)
+    // BIND the defined address to the SERVER's socket
+    int binding = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)); // same as connect parameters
+    if (binding < 0)
     {
         printf("ERROR(bind)\n");
+        return -1;
     }
     else
     {
         printf("socket binded to server's address\n");
     }
 
-    //LISTEN for connections
-    int listening=listen(server_socket,1);// 1 is the backlog: max number of pending connections
-    if(listening<0)
+    // LISTEN for connections
+    int listening = listen(server_socket, 1); // 1 is the backlog: max number of pending connections
+    if (listening < 0)
     {
         printf("ERROR(listen)\n");
+        return -1;
     }
     else
     {
         printf("Listening for incoming connections...\n");
     }
 
-    //ACCEPT the CLIENT connection
+    // ACCEPT the CLIENT connection
     int client_socket;
+    int client_addr_size = sizeof(client_addr);
     // accept(): server doesn't know the client in advance,
     // so kernel fills client_addr when a client connects
-    //so the client addr struct length can change depending on what the kernel inserts
+    // so the client addr struct length can change depending on what the kernel inserts
+    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
+    if (client_socket < 0)
+    {
+        printf("ERROR(accept)\n");
+        return -1;
+    }
+    else
+    {
+        printf("CLIENT connected from IP: %s and PORT: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        // inet_ntoa(): converts IP from binary to readable string (e.g., 127.0.0.1)
+        // ntohs(): converts port from network byte order to host byte order
+    }
 
-    client_socket=accept()
+    char server_msg[2000], client_msg[2000];
+    while (1)
+    {
+        // clear buffers
+        memset(server_msg, '\0', sizeof(server_msg));
+        memset(client_msg, '\0', sizeof(client_msg));
+        // RECV data from CLIENT
+        int recvd = recv(client_socket, client_msg, sizeof(client_msg), 0);
+        if (recvd < 0)
+        {
+            printf("ERROR(recv)\n");
+        }
+        printf("CLIENT's response: %s\n", client_msg);
+        // SEND data to CLIENT
+        printf("Enter Message: ");
+        gets(server_msg);
+        int sent = send(client_socket, server_msg, sizeof(server_msg), 0);
+        if (sent < 0)
+        {
+            printf("ERROR(send)\n");
+        }
+        if (strcmp(client_msg, "exit") == 0)
+            break;
+    }
+    close(server_socket);
+    return 0;
 }
+//To allow retrying connections; wrap the accept() + chat logic inside a while(1) 
